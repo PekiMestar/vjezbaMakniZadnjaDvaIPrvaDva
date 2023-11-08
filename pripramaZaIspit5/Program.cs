2debe840cc289c96a94f6f12e68be104f34e1d30
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-11-08 09:32:04 +0100

    Add files via upload

---
+++ pripramaZaIspit5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pripramaZaIspit5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst;
+
+            Console.WriteLine("Unesite neki tekst: ");
+            
+            tekst = Console.ReadLine();
+
+            Console.WriteLine(tekst.Substring(2) + tekst.Substring(0, tekst.Length -2));
+
+            if(tekst.Length < 5)
+            {
+                Console.WriteLine(tekst);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
